92aad90c9b33b237153989f86c8be6e49fd15d78
Matt G <matt.gaikema@tamu.edu>
2016-05-31 10:07:29 -0500

    No more hotlinking

--- index.php
+++ index.php
@@ -57,7 +57,6 @@
 	</div><!--Carousel-->
 	
 	<h2>Welcome to Math Club</h2>
-	<!--<img id="homer" src="https://dl.dropboxusercontent.com/s/d9rof885miw994y/homer.jpg?dl=0"></img>-->
 	
 	<p>
 		Math Club is an umbrella organization for all undergraduate organizations in the Math Departament
