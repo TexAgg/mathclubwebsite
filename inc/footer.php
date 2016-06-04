e0053a3747c29103834074d585c7b527ced9ba05
Matt G <matt.gaikema@tamu.edu>
2016-06-04 14:15:56 -0400

    ob_end

--- inc/footer.php
+++ inc/footer.php
@@ -1,12 +1,17 @@
-<!--
+<?php
+/*
 	footer.php
 	This is the standard footer that apears in each webpage.
 	
 	Author: Matt Gaikema
 	Date Created: 5/21/16
--->	
+*/	
+?>	
 	<footer>
 		<a href="http://mattgaikema.com/">Matt Gaikema</a> 2016
 	</footer>
 </body>
 </html>
+<?php
+	ob_end_flush();
+?>
