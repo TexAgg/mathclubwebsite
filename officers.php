ccff109841152a1bba822376c3902dcaf54457c5
Matt G <matt.gaikema@tamu.edu>
2016-06-09 13:58:19 -0500

    finish converting to config.xml

--- officers.php
+++ officers.php
@@ -10,33 +10,8 @@
 	$pageTitle = "Officers";
 	$section = "officers";
 	
-	$officers = [];
-	$file = fopen("resources/officers.csv","r");
-	
-	// Add data to array.
-	while(!feof($file))
-	{
-		$officers[] = fgetcsv($file);
-	}
-	// Close the CSV file.
-	fclose($file);
-	
-	// The first element is just column headers;
-	// delete it.
-	array_shift($officers);
-	
-	/*
-		Each element in officers has the following format:
-		
-		$officers[n][0]: The position.
-		$officers[n][1]: The name of the officer.
-		$officers[n][2]: The email of the officer.
-		$officers[n][3]: The url of a picture of the officer.
-		
-		for n in count($officers).
-		$officers[0] is the president, $officers[1] is the vice-president,
-		$officers[2] is the treasurer, and $officer[3] is the webmaster.
-	*/
+	$config = simplexml_load_file("resources/config.xml");
+	$officers = $config->officers;
 	
 	include("inc/header.php");
 ?>
@@ -50,21 +25,21 @@
 	<ul class="media-list">
 		<?php
 			
-			for($i = 0; $i < count($officers); $i++)
-			{
-				echo "<li class=\"media\" id=\"officer".$i."\">";
+			foreach($officers->item as $value)
+			{	
+				echo "<li class=\"media\">";
 				
 				// Media object.
 				echo "<div class=\"media-left\">";
 				echo "<a href=\"#\">";
-				echo "<img class=\"officer-pic media-object\" src=".$officers[$i][3].">";
+				echo "<img class=\"officer-pic media-object\" src=".$value->image.">";
 				echo "</a>";
 				echo "</div>";
 				
 				echo "<div class=\"media-body\">";
-				echo "<h4 class=\"media-heading\">".$officers[$i][0]."</h4>";
-				echo $officers[$i][1];
-				echo "<br><a href=\"mailto:".$officers[$i][2]."\">".$officers[$i][2]."</a>";
+				echo "<h4 class=\"media-heading\">".$value->position."</h4>";
+				echo $value->name;
+				echo "<br><a href=\"mailto:".$value->email."\">".$value->email."</a>";
 				echo  "</div>";
 				
 				echo "</li>";
