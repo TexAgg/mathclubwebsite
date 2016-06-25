ecd2977294b6c28911e92dc88d2dd990c3ddd785
Matt G <matt.gaikema@tamu.edu>
2016-06-24 22:31:07 -0500

    Add bat comment

--- start.bat
+++ start.bat
@@ -1,3 +1,8 @@
+REM This is just a development script that
+REM opens visual studio code,
+REM the commmand prompt,
+REM the file explorer,
+REM and MAMP.
 @echo off
 %SystemRoot%\explorer.exe %~dp0
 start C:\MAMP\MAMP.exe
